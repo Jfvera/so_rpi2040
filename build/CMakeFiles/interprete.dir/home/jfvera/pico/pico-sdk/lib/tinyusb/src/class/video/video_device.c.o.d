e3c51949374c45506aa897d141a57b2ecd3b5b7e
jfvera <jfvera2005@yahoo.es>
2026-02-20 12:28:53 +0100

    Compilación OK

---
+++ build/CMakeFiles/interprete.dir/home/jfvera/pico/pico-sdk/lib/tinyusb/src/class/video/video_device.c.o.d
@@ -0,0 +1,36 @@
+CMakeFiles/interprete.dir/home/jfvera/pico/pico-sdk/lib/tinyusb/src/class/video/video_device.c.o: \
+ /home/jfvera/pico/pico-sdk/lib/tinyusb/src/class/video/video_device.c \
+ /home/jfvera/pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /home/jfvera/pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/tusb_config.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /home/jfvera/pico/pico-sdk/src/common/pico_base_headers/include/pico.h \
+ /home/jfvera/pico/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /home/jfvera/pico/pico-sdk/src/common/pico_base_headers/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stddef.h \
+ /home/jfvera/SO/build/generated/pico_base/pico/version.h \
+ /home/jfvera/pico/pico-sdk/src/common/pico_base_headers/include/pico/config.h \
+ /home/jfvera/SO/build/generated/pico_base/pico/config_autogen.h \
+ /home/jfvera/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/jfvera/pico/pico-sdk/src/rp2040/pico_platform/include/pico/platform.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_platform_compiler/include/pico/platform/compiler.h \
+ /home/jfvera/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/jfvera/pico/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_platform_sections/include/pico/platform/sections.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_platform_panic/include/pico/platform/panic.h \
+ /home/jfvera/pico/pico-sdk/src/rp2_common/pico_platform_common/include/pico/platform/common.h \
+ /home/jfvera/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/jfvera/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/jfvera/pico/pico-sdk/src/common/pico_base_headers/include/pico/error.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdarg.h \
+ /home/jfvera/pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
